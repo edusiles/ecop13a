03581bdb72035d54cc9bd23dee1ffcedc2a2bd98
Eduardo Siles <eduardosiles2003@gmail.com>
2023-05-27 13:24:14 -0300

    Adding exception handling

--- teste/excecao/teste.cpp
+++ teste/excecao/teste.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
+#include <stdexcept>
 
 using namespace std;
 
-int quociente(int n, int m) {
-   return n/m;
+int quociente(int n, int d) {
+   if (d==0) 
+      throw runtime_error("Tentativa de divisao por zero!");
+
+   return n/d;
 }
 
 int main() {
@@ -12,7 +16,11 @@ int main() {
    cout << "Entre com os valores de x e y: ";
    cin >> x >> y;
 
-   cout << "Resultado: " << quociente(x, y) << endl;
+   try {
+      cout << "Resultado: " << quociente(x, y) << endl;
+   } catch (runtime_error &ex) {
+      cerr << "\nOcorreu um erro: " << ex.what() << endl;
+   }
 
    cout << "Programa finalizado!" << endl;
 
